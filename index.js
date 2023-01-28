67c97e7ef89b42fa7ffcbb649a4adf393df5b02a
JuanMendez02 <juan.sebastian.mendez@correounivalle.edu.co>
2023-01-28 10:33:41 -0500

    RetoPullRequest

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 let weatherLondon = require('./weather')
-console.log(weatherLondon.coord.lat)
-console.log(weatherLondon.coord.lon)
+
+
+//let weatherLondon = require('./weather')
+//let weatherLondon = require('./pilotAffected')
+const x = weatherLondon.weather
+
+console.log(`Lat: ${weatherLondon.coord.lat}`);
+console.log(`Lon: ${weatherLondon.coord.lon}`);
+
+//console.log(`Edad: ${weatherLondon.weather[0].pilotAffected[0].age}`);
+console.log(`Las afectados mayores a 18 años son: `);
+for(var i=0; i<3; i++)
+{
+    
+    if (weatherLondon.weather[0].pilotAffected[i].age >= 18){
+
+        console.log(`Nombre: ${weatherLondon.weather[0].pilotAffected[i].name}`);
+    }
+}
+
+
+//console.log(weatherLondon.coord.lat)
+//console.log(weatherLondon.coord.lon)
 
 
 
